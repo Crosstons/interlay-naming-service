adf73b6031aa6c3bac32942d33febfba65451a26
Crostons <shubh1608003@gmail.com>
2023-04-13 15:31:57 +0530

    Pricing section added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Pricing from './components/Pricing';
 function App() {
   return (
     <div>
     <Navbar />
+    <div className='pt-16'>
     <HeroSection />
     <Features />
+    <HowItWorks />
+    <Pricing />
+    </div>
     </div>
   );
 }
