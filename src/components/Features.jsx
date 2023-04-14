23d7042abcea05dc8d4f902244e70a6448083623
Crostons <shubh1608003@gmail.com>
2023-04-14 23:26:54 +0530

    Updated

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,23 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
+import React, { useEffect, useState, useRef } from 'react';
 import { FaRocket, FaLock, FaCode } from 'react-icons/fa';
 
 const FeaturesSection = () => {
   const [isVisible, setIsVisible] = useState([false, false, false]);
-
-  const handleScroll = () => {
-    const windowHeight = window.innerHeight;
-    const featuresElement = document.getElementById('features');
-    const featuresPosition = featuresElement.getBoundingClientRect().top;
-    if (featuresPosition <= windowHeight * 0.8) {
-      setIsVisible([true, true, true]);
-    }
-  };
+  const featureRefs = useRef([React.createRef(), React.createRef(), React.createRef()]);
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          setIsVisible((prevState) => {
+            const updatedState = [...prevState];
+            updatedState[index] = true;
+            return updatedState;
+          });
+        }
+      });
+    }, { threshold: 0.8 });
+
+    featureRefs.current.forEach((ref) => observer.observe(ref.current));
+
+    return () => {
+      featureRefs.current.forEach((ref) => observer.unobserve(ref.current));
+    };
   }, []);
+
+  const featureAnimationClass = (index) => isVisible[index] ? 'transform scale-100 opacity-100' : 'transform scale-90 opacity-0';
+
+  const featureElements = [
+    { Icon: FaRocket, title: 'Feature 1' },
+    { Icon: FaLock, title: 'Feature 2' },
+    { Icon: FaCode, title: 'Feature 3' },
+  ];
 
   return (
     <div id="features" className="bg-gray-800 text-white py-16">
@@ -26,39 +40,20 @@ const FeaturesSection = () => {
           Explore Features
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-                    {/* Feature 1 */}
-          <div className="bg-gray-700 p-6 rounded-lg">
-            <ScrollAnimation animateIn="fadeInUp" duration={1} easing="ease-out" animateOnce={true}>
-              <FaRocket className="text-4xl mb-4" />
-              <h3 className="text-2xl font-semibold mb-4 font-mono">Feature 1</h3>
+          {featureElements.map((feature, index) => (
+            <div
+              ref={featureRefs.current[index]}
+              key={index}
+              className={`bg-gray-700 p-6 rounded-lg transition duration-700 ease-out ${featureAnimationClass(index)}`}
+            >
+              <feature.Icon className="text-4xl mb-4" />
+              <h3 className="text-2xl font-semibold mb-4 font-mono">{feature.title}</h3>
               <p className="text-lg font-mono">
                 Dummy feature description. Elaborate on the benefits and how it
                 can improve the user experience.
               </p>
-            </ScrollAnimation>
-          </div>
-          {/* Feature 2 */}
-          <div className="bg-gray-700 p-6 rounded-lg">
-            <ScrollAnimation animateIn="fadeInUp" duration={1} delay={200} easing="ease-out" animateOnce={true}>
-              <FaLock className="text-4xl mb-4" />
-              <h3 className="text-2xl font-semibold mb-4 font-mono">Feature 2</h3>
-              <p className="text-lg font-mono">
-                Dummy feature description. Elaborate on the benefits and how it
-                can improve the user experience.
-              </p>
-            </ScrollAnimation>
-          </div>
-          {/* Feature 3 */}
-          <div className="bg-gray-700 p-6 rounded-lg">
-            <ScrollAnimation animateIn="fadeInUp" duration={1} delay={400} easing="ease-out" animateOnce={true}>
-              <FaCode className="text-4xl mb-4" />
-              <h3 className="text-2xl font-semibold mb-4 font-mono">Feature 3</h3>
-              <p className="text-lg font-mono">
-                Dummy feature description. Elaborate on the benefits and how it
-                can improve the user experience.
-              </p>
-            </ScrollAnimation>
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
