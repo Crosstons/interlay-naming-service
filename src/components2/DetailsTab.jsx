72d148efd8688bbe53bb2336d39d4810d45f14e7
Crostons <shubh1608003@gmail.com>
2023-05-14 00:28:15 +0530

    Update

--- src/components2/DetailsTab.jsx
+++ src/components2/DetailsTab.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const DetailRow = ({ label, value, actionButton, avatar }) => (
   <div className="flex items-center justify-between">
     <div className="flex items-center justify-start mb-4">
@@ -48,7 +49,15 @@ const DomainDetails = () => {
   const handleCloseTransferClick = () => {
     setShowTransferFields(false);
   };
+  const [showExtendFields, setShowExtendFields] = useState(false);
 
+  const handleExtendClick = () => {
+    setShowExtendFields(!showExtendFields);
+  };
+
+  const handleCloseExtendClick = () => {
+    setShowExtendFields(false);
+  };
   return (
     <div className="bg-gray-700 text-gray-100 font-poppins">
       <div className="container mx-auto py-8">
@@ -98,6 +107,49 @@ const DomainDetails = () => {
           <DetailRow label="Resolver" value={domainDetails.resolver} />
           <DetailRow label="Content" value={domainDetails.content} />
           <DetailRow label="Expires" value={domainDetails.expires} />
+          <DetailRow
+            label="Expires"
+            value={domainDetails.expires}
+            actionButton={
+              <button className="bg-blue-600 hover:bg-blue-700 text-white font-thin py-1 px-6 rounded-lg" onClick={handleExtendClick}>
+                Extend
+              </button>
+            }
+          />
+          {showExtendFields && (
+            <div className="relative">
+              <div className="w-full mt-4">
+                <label htmlFor="newExpiration" className="block text-sm font-medium text-gray-400">
+                  New Expiration
+                </label>
+                <input
+                  type="date"
+                  name="newExpiration"
+                  id="newExpiration"
+                  className="mt-1 block w-full py-2 px-3 border border-gray-600 rounded-md text-gray-100 bg-gray-700"
+                  placeholder="Enter new expiration date"
+                />
+              </div>
+              <div className="absolute top-0 right-0 -mt-4 mr-2">
+                <button
+                  className="bg-red-600 hover:bg-red-700 text-white font-thin py-1 px-2 rounded-lg"
+                  onClick={handleCloseExtendClick}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div className="text-right mt-4">
+                <button
+                  className="bg-blue-600 hover:bg-blue-700 text-white font-thin py-1 px-6 rounded-lg"
+                  onClick={handleExtendClick}
+                >
+                  Extend
+                </button>
+              </div>
+            </div>
+          )}
     </div>
     <h2 className="text-2xl font-bold mb-6">Records</h2>
     <div className="bg-gray-900 rounded-lg shadow-md">
